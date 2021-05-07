31877471103c8625c245bea160301729fa8c2725
asukul <sukul.aritra4@gmail.com>
2021-05-08 02:58:43 +0530

    Microservice

---
+++ currency-exchange-service/target/classes/data.sql
@@ -0,0 +1,12 @@
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple) 
+values(1000, 'USD', 'INR', 65);
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple) 
+values(1001, 'EUR', 'INR', 81);
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple) 
+values(1002, 'AUD', 'INR', 62);
+insert into currency_exchange 
+(id, currency_from, currency_to, conversion_multiple) 
+values(1003, 'CAD', 'INR', 72);
